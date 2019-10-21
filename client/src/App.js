05139a42dccfe7177534aeff3b52d23ca939804a
Tony C <tonychiesa92@gmail.com>
2019-10-21 14:56:38 -0400

    created root route for Home page. removed tesla app shell.

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,12 @@
 import React from "react";
-import Collection from "./containers/Collection";
-import Home from "./containers/Home";
-import Single from "./containers/Single";
-import NewCar from "./containers/NewCar";
-import EditCar from './containers/EditCar';
+import Home from "./pages/Home";
+
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/new-car" component={NewCar} />
-        <Route path="/edit/:id" component={EditCar}/>
-        <Route path="/collection/:id" component={Single} />
-        <Route path="/collection" component={Collection} />
         <Route path="/" component={Home} />
       </Switch>
     </Router>
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-import {Link} from 'react-router-dom';
 
 class Home extends Component {
     render() {
         return (
             <div>
-                <h1>This is my homepage.</h1>
-                <Link to="/collection" >View My Teslas</Link>
+                <h1>Welcome to Virtulogics!</h1>
+                
             </div>
         );
     }
