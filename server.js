883eb2fadda37b3c850306b2f2716f2257f46a10
Mike Smith <smith.mike@mac.com>
2019-10-21 14:06:26 -0400

    upadted the package and server files.

--- server.js
+++ server.js
@@ -41,41 +41,41 @@ connection.on("error", (err) => {
 //     })
 // });
 
-app.get("/api/hosts", function(req, res) {
-    db.HostCpu.find({})
-    .then((allHosts) => {
-        console.log(allHosts);
-        res.json({
-            message: "Requested all Host Metrics",
-            error: false,
-            data: allHosts
-        });
-    }).catch((err) => {
-        console.log(err);
-        res.json({
-            message: err.message,
-            error: true
-        })
-    })
-});
+// app.get("/api/hosts", function(req, res) {
+//     db.HostCpu.find({})
+//     .then((allHosts) => {
+//         console.log(allHosts);
+//         res.json({
+//             message: "Requested all Host Metrics",
+//             error: false,
+//             data: allHosts
+//         });
+//     }).catch((err) => {
+//         console.log(err);
+//         res.json({
+//             message: err.message,
+//             error: true
+//         })
+//     })
+// });
 
-app.post("/api/hosts", function(req, res) {
-    db.HostCpu.create(req.body)
-    .then((newHosts) => {
-        console.log("New Host Metics: ", newHosts);
-        res.json({
-            message: "Successfully created",
-            error: false,
-            data: newHosts
-        })
-    }).catch((err) => {
-        console.log(err);
-        res.json({
-            message: err.message,
-            error: true
-        })
-    })
-});
+// app.post("/api/hosts", function(req, res) {
+//     db.HostCpu.create(req.body)
+//     .then((newHosts) => {
+//         console.log("New Host Metics: ", newHosts);
+//         res.json({
+//             message: "Successfully created",
+//             error: false,
+//             data: newHosts
+//         })
+//     }).catch((err) => {
+//         console.log(err);
+//         res.json({
+//             message: err.message,
+//             error: true
+//         })
+//     })
+// });
 
 // Influx DB connection and routes
 const influx = new Influx.InfluxDB({
