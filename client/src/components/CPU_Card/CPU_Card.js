2a370435a1ed373be584069c5f7596c714e3e564
Seth Randell <sethhrandell@gmail.com>
2019-10-24 16:23:26 -0400

    Added a little stype and added memUsage to props

--- client/src/components/CPU_Card/CPU_Card.js
+++ client/src/components/CPU_Card/CPU_Card.js
@@ -1,16 +1,16 @@
 import React from "react";
 
 const CPU_Card = props => (
-    <div className="card img-container hover">
-    <button className="btn btn-success" >
-    <p>CPU Usage:</p>
-    {props.usagemhz_average}Mhz
+  <div className="card img-container hover">
+    <button className="btn btn-success">
+      <p>CPU Usage:</p>
+      {props.usagemhz_average}Mhz
     </button>
-    <button className="btn btn-success" >
-    <p>CPU Utilization:</p>
-    {props.utilization_average}%
+    <button className="btn btn-success">
+      <p>CPU Utilization:</p>
+      {props.utilization_average}%
     </button>
-    </div>
-  );
-  
-  export default CPU_Card;;
+  </div>
+);
+
+export default CPU_Card;
--- client/src/containers/Hosts/Hosts.js
+++ client/src/containers/Hosts/Hosts.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
-import Moment from "react-moment";
+// import Moment from "react-moment";
 import axios from "axios";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import moment from "moment";
-import LineChart from "../../components/LineChart";
+// import LineChart from "../../components/LineChart";
 import "./Hosts.css";
 import MEM_Card from "../..//components/MEM_Card/MEM_Card";
 import CPU_Card from "../..//components/CPU_Card/CPU_Card";
@@ -12,7 +12,7 @@ class Hosts extends Component {
   state = {
     memData: [],
     cpuData: [],
-    usedMem: 0
+    usedMem: ""
   };
 
   componentDidMount() {
@@ -24,8 +24,12 @@ class Hosts extends Component {
     axios
       .get("/api/host-mem/1")
       .then(hostMemData => {
-        // console.log(hostMemData.data);
-        this.setState({ memData: hostMemData.data.data });
+        const usedMem = hostMemData.data.data.map(data =>
+          ((data.usage_average * data.totalCapacity_average) / 100000).toFixed(
+            2
+          )
+        );
+        this.setState({ memData: hostMemData.data.data, usedMem: usedMem });
       })
       .catch(err => {
         console.log(err);
@@ -48,43 +52,40 @@ class Hosts extends Component {
   render() {
     return (
       <div className="container">
-      <div className="row">
-        <div className="col-md-6">
-          {this.state.memData.map((host, i) => (
-            <div className="card">
-              <div className="card-body">
-                <p>Located in {host.clustername}</p>
-                <p>Host Name: {host.esxhostname}</p>
-                <MEM_Card
-                  totalCapacity_average={host.totalCapacity_average}
-                  usage_average={host.usage_average}
-                />
-                <p>Device polled @ {moment(host.time).format("h:mm:ss a")}</p>
+        <div className="row">
+          <div className="col-md-6">
+            {this.state.memData.map((host, i) => (
+              <div className="card">
+                <div className="card-body">
+                  <p>Located in {host.clustername}</p>
+                  <p>Host Name: {host.esxhostname}</p>
+                  <MEM_Card
+                    totalCapacity_average={host.totalCapacity_average}
+                    usage_average={host.usage_average}
+                  />
+                  <p>Device polled @ {moment(host.time).format("h:mm:ss a")}</p>
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
           </div>
 
-      
-        <div className="col-md-6">
-          {this.state.cpuData.map((host, i) => (
-            <div className="card">
-              <div className="card-body">
-                <p>vCenter {host.vcenter}</p>
-                <p>CPU Ready: {host.utilization_average}</p>
-                <CPU_Card
-                  usagemhz_average={host.usagemhz_average}
-                  utilization_average={host.utilization_average}
-                />
-                <p>Device polled @ {moment(host.time).format("h:mm:ss a")}</p>
+          <div className="col-md-6">
+            {this.state.cpuData.map((host, i) => (
+              <div className="card">
+                <div className="card-body">
+                  <p>vCenter {host.vcenter}</p>
+                  <p>CPU Ready: {host.utilization_average}</p>
+                  <CPU_Card
+                    usagemhz_average={host.usagemhz_average}
+                    utilization_average={host.utilization_average}
+                  />
+                  <p>Device polled @ {moment(host.time).format("h:mm:ss a")}</p>
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
           </div>
         </div>
-
       </div>
-    
     );
   }
 }
