b2af6ae3f2470937f5354d1efab68193a6f816a8
Ryan Sims <ryan.sims@snapnurse.com>
2019-10-24 20:06:38 -0400

    Built wireframe in Boostrap with placeholder images and date

--- client/src/containers/Home.js
+++ client/src/containers/Home.js
@@ -88,18 +88,13 @@ class Home extends Component {
     <img src="https://via.placeholder.com/300" className="card-img-top" alt="..." />
     <div className="card-body">
       <h5 className="card-title">Gold Plan</h5>
-      <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+      <p className="card-text">This is a wide card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
       <p className="card-text"><small className="text-muted">Per User Per Month</small></p>
     </div>
   </div>
 </div>
 
   </>
-
-
-
-
-
         );
     }
 }
