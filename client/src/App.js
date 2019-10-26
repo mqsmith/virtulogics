c750974ac5bf97e4fade6b9128a7c696cc880ea7
Tony C <tonychiesa92@gmail.com>
2019-10-25 22:30:54 -0400

    top and side nav bars formated correctly within App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,23 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./containers/Home";
-import Nav from "./components/Nav";
+import Nav from "./components/Nav/Nav";
+import Sidebar from "./components/Sidebar/Sidebar";
 import Collection from "./containers/Collection";
+import './App.css';
 
 function App() {
   return (
     <Router>
       <div>
         <Nav />
+        <Sidebar />
+        <div id="page-wrap">
         <Switch>
           <Route path="/collection" component={Collection} />
           <Route path="/" component={Home} />
         </Switch>
+        </div>
       </div>
     </Router>
   );
