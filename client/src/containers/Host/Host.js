e305191ad38ff0a467c2c6627b18c08c0fbe68ab
Seth Randell <sethhrandell@gmail.com>
2019-10-27 22:08:54 -0400

    Got loading component to laod before the axiso call on the hosts and host page

--- client/src/containers/Host/Host.js
+++ client/src/containers/Host/Host.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import moment from "moment";
-// import LineChart from "../../components/LineChart";
+import Loading from "../../components/Loading/Loading";
 // import "./Hosts.css";
 import MEM_Card from "../..//components/MEM_Card/MEM_Card";
 import CPU_Card from "../..//components/CPU_Card/CPU_Card";
@@ -11,87 +11,93 @@ import CPU_Card from "../..//components/CPU_Card/CPU_Card";
 class Host extends Component {
   state = {
     allData: [],
-    usedMem: ""
+    loading: true
   };
 
   componentDidMount() {
     this.getBoth();
   }
-  
+
   getBoth = () => {
     axios
-    .get("/api/host/cpu-mem/1/" + this.props.match.params.esxhostname)
-    .then(allData => {
-      let  obj = allData.data
-      const array = Object.values(obj)
-      // console.log(array);
-      this.setState({ allData: array });
-      console.log(this.state.allData[0].totalCapacity_average)
-      console.log(this.state.allData[0].mem_usage_average)
-      console.log((this.state.allData[0].totalCapacity_average * this.state.allData[0].mem_usage_average / 100 * 1024))
-      
-    })
-    .catch(err => {
-      console.log(err);
-    });
-};
-
+      .get("/api/host/cpu-mem/1/" + this.props.match.params.esxhostname)
+      .then(allData => {
+        let obj = allData.data;
+        const array = Object.values(obj);
+        // console.log(array);
+        this.setState({ allData: array, loading: false });
+        console.log(this.state.allData[0].totalCapacity_average);
+        console.log(this.state.allData[0].mem_usage_average);
+        console.log(
+          ((this.state.allData[0].totalCapacity_average *
+            this.state.allData[0].mem_usage_average) /
+            100) *
+            1024
+        );
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
   render() {
+    let content;
+    if (this.state.loading) {
+      content = (
+        <div className="loading">
+          <Loading />
+        </div>
+      );
+    } else {
     return (
       <div className="container">
         <div className="row">
           <div className="col-md-12">
             {this.state.allData.map((host, i) => (
               <div className="card card-wrapper">
-                <div  key={host.moid} className="card main-card">
-                <div className="row header-row">
-          
-                <div className="col-md-7 top-left">
-                <Link to="/hosts">
-                <button className="btn-dark btn-sm">
-                 Go back to your Host view
-                </button>     
-                </Link>
+                <div key={host.moid} className="card main-card">
+                  <div className="row header-row">
+                    <div className="col-md-7 top-left">
+                      <Link to="/hosts">
+                        <button className="btn-dark btn-sm">
+                          Go back to your Host view
+                        </button>
+                      </Link>
+                    </div>
+                    <div className="col-md-4 top-right">
+                      <p>vCenter {host.vcenter}</p>
+                      <p>CPU Ready: {host.utilization_average}</p>
+                    </div>
                   </div>
-                  
-                <div className="col-md-4 top-right">
-                  <p>vCenter {host.vcenter}</p>
-                  <p>CPU Ready: {host.utilization_average}</p>
-                </div>
-                </div>
-             
                   <div className="row button-row">
-                  <div className="col-md-3 top-left">
-                  <img src="https://assets.webiconspng.com/uploads/2017/09/Server-PNG-Image-23361.png" />
-                  <p>Host: {host.esxhostname}</p>
-                  <p>Cluster: {host.clustername}</p>
+                    <div className="col-md-3 top-left">
+                      <img src="https://assets.webiconspng.com/uploads/2017/09/Server-PNG-Image-23361.png" />
+                      <p>Host: {host.esxhostname}</p>
+                      <p>Cluster: {host.clustername}</p>
+                    </div>
+                    <div className="col-md-4 mem-col">
+                      <MEM_Card
+                        totalCapacity_average={host.totalCapacity_average}
+                        usage_average={host.usage_average}
+                      />
+                    </div>
+                    <div className="col-md-4 cpu-col">
+                      <CPU_Card
+                        usagemhz_average={host.usagemhz_average}
+                        utilization_average={host.cpu_usage_average}
+                      />
+                    </div>
                   </div>
-                  <div className="col-md-4 mem-col">
-                  <MEM_Card
-                    totalCapacity_average={host.totalCapacity_average}
-                    usage_average={host.usage_average}
-                  />
-                   </div>
-                   <div className="col-md-4 cpu-col">
-                  <CPU_Card
-                    usagemhz_average={host.usagemhz_average}
-                    utilization_average={host.cpu_usage_average}
-                  />
-                   </div>
-                   </div>
-                    <p>Device polled @ {moment(host.time).format("h:mm:ss a")}</p>
-              </div>
-                 
+                  <p>Device polled @ {moment(host.time).format("h:mm:ss a")}</p>
                 </div>
-       
+              </div>
             ))}
           </div>
-
         </div>
       </div>
     );
   }
+  return <div>{content}</div>;
 }
-
+}
 
 export default Host;
--- client/src/containers/Hosts/Hosts.js
+++ client/src/containers/Hosts/Hosts.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import moment from "moment";
-// import LineChart from "../../components/LineChart";
+import Loading from "../../components/Loading/Loading";
 import "./Hosts.css";
 import MEM_Card from "../..//components/MEM_Card/MEM_Card";
 import CPU_Card from "../..//components/CPU_Card/CPU_Card";
@@ -11,89 +11,94 @@ import CPU_Card from "../..//components/CPU_Card/CPU_Card";
 class Hosts extends Component {
   state = {
     allData: [],
-    usedMem: ""
+    loading: true
   };
 
   componentDidMount() {
     this.getBoth();
   }
-  
+
   getBoth = () => {
     axios
-    .get("/api/host/cpu-mem/1")
-    .then(allData => {
-      let  obj = allData.data
-      const array = Object.values(obj)
-      // console.log(array);
-      this.setState({ allData: array });
-      console.log(this.state.allData[0].totalCapacity_average)
-      console.log(this.state.allData[0].mem_usage_average)
-      console.log((this.state.allData[0].totalCapacity_average * this.state.allData[0].mem_usage_average / 100 * 1024))
-      
-    })
-    .catch(err => {
-      console.log(err);
-    });
-};
-
+      .get("/api/host/cpu-mem/1")
+      .then(allData => {
+        let obj = allData.data;
+        const array = Object.values(obj);
+        // console.log(array);
+        this.setState({ allData: array, loading: false });
+        console.log(this.state.allData[0].totalCapacity_average);
+        console.log(this.state.allData[0].usage_average);
+        console.log(
+          ((this.state.allData[0].totalCapacity_average *
+            this.state.allData[0].usage_average) /
+            100) *
+            100000
+        );
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
   render() {
-    return (
-      <div className="container">
-        <div className="row">
-          <div className="col-md-12">
-            {this.state.allData.map((host, i) => (
-              <div className="card card-wrapper">
-              
-                <div  key={host.moid} className="card main-card" >
-               
-                <div className="row header-row">
-                <div className="col-md-7 top-left">
-                <Link to={`/host/${host.esxhostname}`}>
-                <button className="btn-dark btn-sm">
-                 Click to view host details
-                </button>     
-                </Link>
-                  </div>
-                  
-                <div className="col-md-4 top-right">
-                  <p>vCenter {host.vcenter}</p>
-                  <p>CPU Ready: {host.utilization_average}</p>
-                </div>
-                </div>
-             
-                  <div className="row button-row">
-                  <div className="col-md-3 top-left">
-                  <img src="https://assets.webiconspng.com/uploads/2017/09/Server-PNG-Image-23361.png" />
-                  <p>Host: {host.esxhostname}</p>
-                  <p>Cluster: {host.clustername}</p>
-                  </div>
-                  <div className="col-md-4 mem-col">
-                  <MEM_Card
-                    totalCapacity_average={host.totalCapacity_average}
-                    usage_average={host.usage_average}
-                  />
-                   </div>
-                   <div className="col-md-4 cpu-col">
-                  <CPU_Card
-                    usagemhz_average={host.usagemhz_average}
-                    utilization_average={host.cpu_usage_average}
-                  />
-                   </div>
-                   </div>
-                    <p>Device polled @ {moment(host.time).format("h:mm:ss a")}</p>
-                    
-              </div>
-      
-                </div>
-       
-            ))}
-          </div>
-
+    let content;
+    if (this.state.loading) {
+      content = (
+        <div className="loading">
+          <Loading />
         </div>
-      </div>
-    );
+      );
+    } else {
+      return (
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              {this.state.allData.map((host, i) => (
+                <div className="card card-wrapper">
+                  <div key={host.moid} className="card main-card">
+                    <div className="row header-row">
+                      <div className="col-md-7 top-left">
+                        <Link to={`/host/${host.esxhostname}`}>
+                          <button className="btn-dark btn-sm">
+                            Click to view host details
+                          </button>
+                        </Link>
+                      </div>
+                      <div className="col-md-4 top-right">
+                        <p>vCenter {host.vcenter}</p>
+                        <p>CPU Ready: {host.utilization_average}</p>
+                      </div>
+                    </div>
+                    <div className="row button-row">
+                      <div className="col-md-4 top-left">
+                        <img src="https://assets.webiconspng.com/uploads/2017/09/Server-PNG-Image-23361.png" />
+                        <p>Host: {host.esxhostname}</p>
+                        <p>Cluster: {host.clustername}</p>
+                      </div>
+                      <div className="col-md-4 mem-col">
+                        <MEM_Card
+                          totalCapacity_average={host.totalCapacity_average}
+                          usage_average={host.usage_average}
+                        />
+                      </div>
+                      <div className="col-md-4 cpu-col">
+                        <CPU_Card
+                          usagemhz_average={host.usagemhz_average}
+                          utilization_average={host.cpu_usage_average}
+                        />
+                      </div>
+                    </div>
+                    <p>
+                      Device polled @ {moment(host.time).format("h:mm:ss a")}
+                    </p>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      );
+    }
+    return <div>{content}</div>;
   }
 }
-
-
 export default Hosts;
