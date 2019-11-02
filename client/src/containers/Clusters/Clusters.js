ef5e716e213dc0295628cae5f45e332432a3a543
Seth Randell <sethhrandell@gmail.com>
2019-11-01 22:45:05 -0400

    Added N+CPU and played arroudn ith the formating

--- client/src/containers/Clusters/Clusters.js
+++ client/src/containers/Clusters/Clusters.js
@@ -1,12 +1,11 @@
 import React, { Component } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
-import PieComponent from "../..//components/Pie/Pie"
+import PieComponent from "../..//components/Pie/Pie";
 import Loading from "../../components/Loading/Loading";
 import Dual_Button_Card from "../..//components/Dual_Button_Card/";
-import ClusterChartContainer from "../../components/ClusterChart/ClusterChartContainer"
+import ClusterChartContainer from "../../components/ClusterChart/ClusterChartContainer";
 import "./Clusters.css";
-
 
 class Clusters extends Component {
   state = {
@@ -16,9 +15,7 @@ class Clusters extends Component {
     label: [],
     data: [],
     singlevcentername: [],
-    singleclustername: [],
-
-
+    singleclustername: []
   };
 
   componentDidMount() {
@@ -38,60 +35,97 @@ class Clusters extends Component {
         for (let i = 0; i < this.state.allData.length; i++) {
           // console.log(this.state.allData[i].usage_average)
           combined.push(this.state.allData[i].usage_average);
-
         }
         // console.log(combined);
-       
+
         // console.log(array);
         // let cluster = this.state.allData.filter(
         //   data => data.usage_average === "lab-esxi-01.vdilab.int"
-     
+
         // );
         const memData = this.state.allData.map(data => data.usage_average);
         let memTotal = 0;
-          //Add the to values together to make the total
-          for ( let i =0; i < memData.length; i++ ) {
-            memTotal += memData[i]
-          }
+        //Add the to values together to make the total
+        for (let i = 0; i < memData.length; i++) {
+          memTotal += memData[i];
+        }
         const cpuData = this.state.allData.map(data => data.cpu_usage_average);
         let cputotal = 0;
-          //Add the to values together to make the total
-          for ( let e =0; e < cpuData.length; e++ ) {
-            cputotal += cpuData[e]
-          }
-       
+        //Add the to values together to make the total
+        for (let e = 0; e < cpuData.length; e++) {
+          cputotal += cpuData[e];
+        }
+
         const labelData = this.state.allData.map(data => data.esxhostname);
         const clusterName = this.state.allData.map(data => data.clustername);
         const singleclustername = [...new Set(clusterName)];
         const vcenterName = this.state.allData.map(data => data.vcenter);
         const singlevcentername = [...new Set(vcenterName)];
-        const memCapcity =  this.state.allData.map(data => data.totalCapacity_average);
+        const memCapcity = this.state.allData.map(
+          data => data.totalCapacity_average
+        );
         let clusterMemTotal = 0;
-          //Add the to values together to make the total
-          for ( let f =0; f < memCapcity.length; f++ ) {
-            clusterMemTotal += memCapcity[f]
-          }
+        //Add the to values together to make the total
+        for (let f = 0; f < memCapcity.length; f++) {
+          clusterMemTotal += memCapcity[f];
+        }
         let hostmemory = 28383;
-        //Uses real data but may be a liittle off
-        // let totalhostcapacity = clusterMemTotal / 1000
-
-        let totalhosts = labelData.length
-        let totalhostmemory = ((hostmemory * memTotal)/ 100000).toFixed(2);
+        let totalhosts = labelData.length;
+        let totalclustermemory = ((hostmemory * memTotal) / 100000).toFixed(2);
         console.log("This is the MEM total");
-        console.log(totalhostmemory);
-        let n1 = ((64 - totalhostmemory) / 32).toFixed(2);
-        console.log(n1);
-        console.log(clusterMemTotal / 1000 / 32 - totalhostmemory);
+        console.log(totalclustermemory);
+        let n1mem = ((64 - totalclustermemory) / 32).toFixed(2);
+        console.log(n1mem);
+
+        const cpuUsage = this.state.allData.map(
+          data => data.usagemhz_average
+        );
+        let clusterCpuTotal = 0;
+        //Add the to values together to make the total
+        for (let f = 0; f < cpuUsage.length; f++) {
+          clusterCpuTotal += cpuUsage[f];
+        }
+
+        const cpuUsagePer = this.state.allData.map(
+          data => data.cpu_usage_average
+        );
+        let clusterusagetotal = 0;
+        //Add the to values together to make the total
+        for (let g = 0; g < cpuUsagePer.length; g++) {
+          clusterusagetotal += cpuUsagePer[g];
+        }
       
+        let hostcpu = 17.552;
+        let totalcpu = 35.104;
+
+        let totalclustercpu = (
+          (totalcpu - clusterCpuTotal / 1000) /
+          hostcpu
+        ).toFixed(2);
+        console.log("This is the CPU total");
+        console.log(clusterCpuTotal);
+        console.log(totalclustercpu);
+        let n1cpu = totalclustercpu;
+        console.log(clusterusagetotal);  
+     
+
         // memTotal.push(this.state.data);
         // labelData.push("Total");
 
         //Set state
-        this.setState({ n1: n1, cputotal: cputotal, totalmemoryusage: memTotal, label: labelData, data: memData, singleclustername: singleclustername,  singlevcentername: singlevcentername});
-   
-        // console.log(labelData.length);
-        // console.log(memTotal);
-     
+        this.setState({
+          clusterusagetotal: clusterusagetotal,
+          clustercputotal: clusterCpuTotal,
+          n1cpu: n1cpu,
+          totalclustermemory: totalclustermemory,
+          n1mem: n1mem,
+          cputotal: cputotal,
+          totalmemoryusage: memTotal,
+          label: labelData,
+          data: memData,
+          singleclustername: singleclustername,
+          singlevcentername: singlevcentername
+        });
       })
       .catch(err => {
         console.log(err);
@@ -108,61 +142,79 @@ class Clusters extends Component {
     } else {
       return (
         <div className="container">
-          <div className="row">
-            <div className="col-md-12">
-              <div className="card card-wrapper">
-                <div className="card main-card">
+        <div className="card card-wrapper">
+        <div className="card main-card">
                   <div className="row header-row">
                     <div className="col-md-7 top-left">
                       <Link to="/hosts">
                         <button className="btn-dark btn-sm">
                           Click to view host details
-                          </button>
+                        </button>
                       </Link>
                     </div>
-                    <div className="col-md-4 top-right">
                     </div>
-                    <div className="row button-row">
-                      <div className="col-md-4 top-left">
+                  <div className="row">
+                  <div className="col-md-4 top-left">
                         <img src="/img/cluster.png" />
                         <div className="host-text-box">
-                        <p>Cluster: {this.state.singleclustername}</p>
-                        <p>vCenter: 
-                        {this.state.singlevcentername}</p>
-                        <p>Number of ESXi Hosts: {this.state.label.length}</p>
-                        <p>N+RAM: {this.state.n1}</p>
+                          <p>Cluster: {this.state.singleclustername}</p>
+                          <p>
+                            vCenter:
+                            {this.state.singlevcentername}
+                          </p>
+                          <p>Number of ESXi Hosts: {this.state.label.length}</p>
+                          <p>N+RAM: {this.state.n1mem}</p>
+                          <p>N+CPU: {this.state.n1cpu}</p>
                         </div>
                       </div>
-                      <div className="col-md-5 doughnut-chart">
-                      <p>Cluster memory usage by host</p>
-                      <PieComponent {...this.state}
-                       />
+         
+                  <div className="col-md-5 doughnut-chart">
+                        <p>Cluster memory usage by host</p>
+                        <PieComponent {...this.state} />
                       </div>
-                     
-                      <div className="col-md-3 cpu-col">
-                      <Dual_Button_Card
-                          title="Cluster CPU/MEM Usage"
-                          text1="CPU Usage (%):"
+                  <div className="col-md-3 cpu-col">
+                        <Dual_Button_Card
+                          title="Cluster Memory Usage"
+                          text1="MEM Usage (GB):"
                           text2="MEM Usage (%):"
-                          firstButton={this.state.cputotal}
+                          firstButton={this.state.totalclustermemory}
                           secondButton={this.state.totalmemoryusage}
                         />
-                     
                       </div>
-                    </div>
-                    <div className="col-md-4 mem-col">
-
-                    </div>
-                    <div className="col-md-4 cpu-col">
-
-                    </div>
                   </div>
-                  <ClusterChartContainer/>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+                  <div className="row header-row">
+                    <div className="col-md-7 top-left">
+               
+                    </div>
+                    </div>
+                  <div className="row">
+                 
+                      <div className="col-md-4 cpu-col"></div>
+                      <div className="col-md-5 doughnut-chart">
+                        <p>Cluster CPU usage by host</p>
+                        <PieComponent {...this.state} />
+                      </div> 
+                      <div className="col-md-3 cpu-col">
+                        <Dual_Button_Card
+                          title="Cluster CPU Usage"
+                          text1="CPU Usage (Mhz):"
+                          text2="CPU Usage (%):"
+                          firstButton={this.state.clustercputotal}
+                          secondButton={this.state.clusterusagetotal}
+                        />
+                      </div>
+                        </div>
+                        <ClusterChartContainer />
+                      </div>
+                      {/* <div className="col-md-5 doughnut-chart">  </div> */}
+                  
+                
+                 
+                  </div>
+               
+                  </div>
+               
+           
       );
     }
     return <div>{content}</div>;
