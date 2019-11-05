4ebda6f7ae8cbe3508eab20a4da4d08dfe91c5da
Mike Smith <smith.mike@mac.com>
2019-11-05 15:00:34 -0500

    changed the host view to a table instead of cards.

--- client/src/containers/Hosts/Hosts.js
+++ client/src/containers/Hosts/Hosts.js
@@ -48,159 +48,182 @@ class Hosts extends Component {
         console.log(err);
       });
   };
-  render() {
-    let content;
-    if (this.state.loading) {
-      content = (
-        <div className="loading">
-          <Loading />
-        </div>
-      );
-    } else {
+
+  renderTableHeader() {
+    return (
+      <tr key="headers">
+        <th>Host Name</th>
+        <th>Usage Average</th>
+        <th>Memory Usage (GB)</th>
+        <th>CPU Usage Average</th>
+      </tr>
+    );
+  }
+
+  renderTableData() {
+    return this.state.allData.map((host, i) => {
       return (
-        <div className="wrapper">
-          {this.state.allData.map((host, i) => (
-            <div className="host">
-              <div className="title-row shadow mb-3 bg-white rounded">
-                <p className="page-title">Host: {host.esxhostname}</p>
-              </div>
-
-              <div className="row">
-                <div className="col-md-2">
-                  <Link to={`/host/${host.esxhostname}`}>
-                    <button className="btn-dark btn-sm link-button">
-                      Click to view "Host" details
-                    </button>
-                  </Link>
-
-                  <div className="card host-card">
-                    <div id="inner" className="card-header">
-                      Host Information:
-                    </div>
-                    <p className="bold-text host-text">vCenter:</p>
-                    <p className="host-text">{host.vcenter}</p>
-                    <p className="bold-text host-text">Cluster:</p>
-                    <p className="host-text">{host.clustername}</p>
-                  </div>
-                </div>
-
-                <div className="col-md-2">
-                  <div className="card host-card">
-                    <div id="inner" className="card-header">
-                      Memory Usage
-                    </div>
-                    <CircularProgressbar
-                      value={host.usage_average}
-                      text={
-                        <tspan
-                          className="progress"
-                          dy={needDominantBaselineFix ? -15 : 0}
-                        >
-                          {host.usage_average}%
-                        </tspan>
-                      }
-                    />
-                  </div>
-                </div>
-
-                <div className="col-md-2">
-                  <div className="card host-card">
-                    <div id="inner" className="card-header">
-                      MEM Usage (GB)
-                    </div>
-                    <CircularProgressbar
-                      value={(
-                      (host.totalCapacity_average * host.usage_average) /
-                      100000
-                    ).toFixed(2)}
-                      text={
-                        <tspan
-                          className="progress"
-                          dy={needDominantBaselineFix ? -15 : 0}
-                        >
-                          {(
-                      (host.totalCapacity_average * host.usage_average) /
-                      100000
-                    ).toFixed(2)}GB
-                        </tspan>
-                      }
-                    />
-                  </div>
-                </div>
-
-                <div className="col-md-2">
-                  <div className="card host-card">
-                    <div id="inner" className="card-header">
-                      CPU Usage
-                    </div>
-                    <CircularProgressbar
-                      value={host.cpu_usage_average}
-                      text={
-                        <tspan
-                          className="progress"
-                          dy={needDominantBaselineFix ? -15 : 0}
-                        >
-                          {host.cpu_usage_average}%
-                        </tspan>
-                      }
-                    />
-                  </div>
-                </div>
-
-
-                <div className="col-md-4">
-                  <div className="card host-card">
-                    <div id="inner" className="card-header">
-                      Host Performance Stats:
-                    </div>
-                    <div className="row">
-                      <div className="col">
-                        <p className="triple-label">CPU Ready</p>
-                        <p className="triple">{host.utilization_average}</p>
-                      </div>
-                      <div className="col">
-                        <p className="triple-label">CO-Stop</p>
-                        <p className="triple">{host.costop_summation}</p>
-                      </div>
-                      <div className="col">
-                        <p className="triple-label">Swap IN Avg.</p>
-                        <p className="triple">{host.swapinRate_average}</p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <p className="polled">
-                Device polled @ {moment(host.time).format("h:mm:ss a")}
-              </p>
-            </div>
-          ))}
-        </div>
+        <tr key={host.esxhostname}>
+          <td>{host.esxhostname}</td>
+          <td>{(host.usage_average).toFixed(2)}</td>
+          <td>
+            {(
+              (host.totalCapacity_average * host.usage_average) /
+              100000
+            ).toFixed(2)}
+          </td>
+          <td>{(host.cpu_usage_average).toFixed(2)}</td>
+        </tr>
       );
-    }
-    return <div>{content}</div>;
+    });
+  }
+
+  render() {
+    return (
+      <>
+        {this.state.loading ? (
+          <div className="loading">
+            <Loading />
+          </div>
+        ) : (
+          <div className="wrapper">
+            <div>
+              <h1 id="title">Host Info</h1>
+              <table id="hosts">
+                <tbody>
+                  {this.renderTableHeader()}
+                  {this.renderTableData()}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        )}
+      </>
+    );
   }
 }
 export default Hosts;
 
 {
-  /* <div className="col-md-4 cpu-col">
-                  <Dual_Button_Card
-                    title="Host CPU Usage"
-                    text1="CPU Usage (Mhz):"
-                    text2="CPU Usage (%):"
-                    firstButton={host.usagemhz_average}
-                    secondButton={host.cpu_usage_average}
-                  />
-                  <Dual_Button_Card
-                    title="Host Memory Usage"
-                    text1="MEM Usage (GB):"
-                    text2="MEM Usage (%):"
-                    firstButton={(
-                      (host.totalCapacity_average * host.usage_average) /
-                      100000
-                    ).toFixed(2)}
-                    secondButton={host.usage_average}
-                  />
+  /* {this.state.allData.map((host, i) => (
+              <div className="host">
+                <div className="title-row shadow mb-3 bg-white rounded">
+                  <p className="page-title">Host: {host.esxhostname}</p>
+                </div>
+
+                <div className="row">
+                  <div className="col-md-2">
+                    <Link to={`/host/${host.esxhostname}`}>
+                      <button className="btn-dark btn-sm link-button">
+                        View Single Host Details
+                      </button>
+                    </Link>
+
+                    <div className="card host-card">
+                      <div id="inner" className="card-header">
+                        Host Information:
+                      </div>
+                      <p className="bold-text host-text">vCenter:</p>
+                      <p className="host-text">{host.vcenter}</p>
+                      <p className="bold-text host-text">Cluster:</p>
+                      <p className="host-text">{host.clustername}</p>
+                    </div>
+                  </div>
+
+                  <div className="col-md-2">
+                    <div className="card host-card">
+                      <div id="inner" className="card-header">
+                        Memory Usage
+                      </div>
+                      <CircularProgressbar
+                        value={host.usage_average}
+                        text={
+                          <tspan
+                            className="progress"
+                            dy={needDominantBaselineFix ? -15 : 0}
+                          >
+                            {host.usage_average}%
+                          </tspan>
+                        }
+                      />
+                    </div>
+                  </div>
+
+                  <div className="col-md-2">
+                    <div className="card host-card">
+                      <div id="inner" className="card-header">
+                        MEM Usage (GB)
+                      </div>
+                      <CircularProgressbar
+                        value={(
+                          (host.totalCapacity_average * host.usage_average) /
+                          100000
+                        ).toFixed(2)}
+                        text={
+                          <tspan
+                            className="progress"
+                            dy={needDominantBaselineFix ? -15 : 0}
+                          >
+                            {(
+                              (host.totalCapacity_average *
+                                host.usage_average) /
+                              100000
+                            ).toFixed(2)}
+                            GB
+                          </tspan>
+                        }
+                      />
+                    </div>
+                  </div>
+
+                  <div className="col-md-2">
+                    <div className="card host-card">
+                      <div id="inner" className="card-header">
+                        CPU Usage
+                      </div>
+                      <CircularProgressbar
+                        value={host.cpu_usage_average}
+                        text={
+                          <tspan
+                            className="progress"
+                            dy={needDominantBaselineFix ? -15 : 0}
+                          >
+                            {host.cpu_usage_average}%
+                          </tspan>
+                        }
+                      />
+                    </div>
+                  </div>
+
+                  <div className="col-md-4">
+                    <div className="card host-card">
+                      <div id="inner" className="card-header">
+                        Host Performance Stats:
+                      </div>
+                      <div className="row">
+                        <div className="col">
+                          <p className="triple-label">CPU Ready</p>
+                          <p className="triple">{host.utilization_average}</p>
+                        </div>
+                        <div className="col">
+                          <p className="triple-label">CO-Stop</p>
+                          <p className="triple">{host.costop_summation}</p>
+                        </div>
+                        <div className="col">
+                          <p className="triple-label">Swap IN Avg.</p>
+                          <p className="triple">{host.swapinRate_average}</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
                 </div> */
+}
+{
+  /* <p className="polled">
+                  Device polled @ {moment(host.time).format("h:mm:ss a")}
+                </p> */
+}
+
+{
+  /* )} */
 }
