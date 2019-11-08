046baa5ee8ea33cd0788477ef99baa8099381635
Mike Smith <smith.mike@mac.com>
2019-11-08 02:11:08 -0500

    made the close button just an x in the corner

--- client/src/components/SideBarWide/SideBarWide.js
+++ client/src/components/SideBarWide/SideBarWide.js
@@ -8,29 +8,30 @@ const SideBarWide = props => {
         <div className="sidebar-sticky">
           <br></br>
           <ul className="nav flex-column">
-            <button
-              type="button"
-              className="btn btn-dark btn-sm"
-              onClick={props.navHandler}
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                class="feather feather-x-circle"
+            <div className="text-right">
+              <button
+                type="button"
+                className="btn-dark toggle-button"
+                onClick={props.navHandler}
               >
-                <circle cx="12" cy="12" r="10" />
-                <line x1="15" y1="9" x2="9" y2="15" />
-                <line x1="9" y1="9" x2="15" y2="15" />
-              </svg>
-              Close
-            </button>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  class="feather feather-x"
+                >
+                  <line x1="18" y1="6" x2="6" y2="18" />
+                  <line x1="6" y1="6" x2="18" y2="18" />
+                </svg>
+              </button>
+            </div>
+
             <li className="nav-item">
               <a className="nav-link active" href="/clusters">
                 <svg
