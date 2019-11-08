9cb26b51cf2e773c51a642d2466fcd1d4c4dc910
Seth Randell <sethhrandell@gmail.com>
2019-11-08 15:37:37 -0500

    Tweaked te side bar and change the closing button to a div

--- client/src/components/SideBarWide/SideBarWide.js
+++ client/src/components/SideBarWide/SideBarWide.js
@@ -9,27 +9,13 @@ const SideBarWide = props => {
           <br></br>
           <ul className="nav flex-column">
             <div className="text-right animated fadeInRight delay-1s">
-              <button
-                type="button"
-                className="btn-dark toggle-button"
+              <div
+       
+                className="toggle-x"
                 onClick={props.navHandler}
               >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  className="feather feather-x"
-                >
-                  <line x1="18" y1="6" x2="6" y2="18" />
-                  <line x1="6" y1="6" x2="18" y2="18" />
-                </svg>
-              </button>
+                X
+              </div>
             </div>
 
             <li className="nav-item animated fadeInRight delay-1s">
